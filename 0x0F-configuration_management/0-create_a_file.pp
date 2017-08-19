f86ef46fec38fa3b1a1c20ad3bc3de917c4147d3
jarehec <bone1198@gmail.com>
2017-08-19 00:40:52 +0000

    re-added ensure

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 #writes 'I love Puppet' in /tmp/holberton
 file { 'create_a_file':
-path      => '/tmp/holberton',
-mode      => '0744',
-group     => 'www-data',
-owner     => 'www-data',
-content   => 'I love Puppet'
+ensure		=>  file,
+path		=> '/tmp/holberton',
+mode		=> '0744',
+group		=> 'www-data',
+owner		=> 'www-data',
+content		=> 'I love Puppet'
 }
