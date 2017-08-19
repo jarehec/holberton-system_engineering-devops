22c4dc292989263830974cbd07f5ecf5a01bbf79
jarehec <bone1198@gmail.com>
2017-08-19 01:22:15 +0000

    align arrows

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #writes 'I love Puppet' in /tmp/holberton
 
 file { 'create_a_file':
-ensure      =>  file,
-path        => '/tmp/holberton',
-mode        => '0744',
-group       => 'www-data',
-owner       => 'www-data',
-content     => 'I love Puppet'
+ensure  =>  file,
+path    => '/tmp/holberton',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data',
+content => 'I love Puppet'
 }
