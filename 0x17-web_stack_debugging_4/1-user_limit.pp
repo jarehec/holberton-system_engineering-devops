78765bc14593cc78dceee267b41f192f42913f12
jarehec <bone1198@gmail.com>
2017-09-20 18:58:48 +0000

    initial commit

---
+++ 0x17-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Fix for nginx
+exec { 'increase file limit':
+command  => 'awk \'$1 == "holberton" { $4=50000 }1\' /etc/security/limits.conf > tmp && mv tmp /etc/security/limits.conf',
+path     => ['/usr/bin', '/bin', '/usr/sbin'],
+provider => 'shell'
+}
