3ab94e7c966f26ff1b580c093ee7548d5ec00524
jarehec <bone1198@gmail.com>
2017-09-21 22:35:25 +0000

    initial commit

---
+++ 0x18-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+returns the subscriber count of a given subreddit
+"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    url = "https://reddit.com/r/{}/about.json".format(subreddit)
+    res = get(url, headers={'User-agent': 'hAxr'}).json()
+    subs = res.get('data').get('subscribers')
+    return (subs) if subs is not None else 0
---
+++ 0x18-api_advanced/README.md
@@ -0,0 +1,10 @@
+#  Holberton School -0x18. API advanced 
+
+
+## Environment
+* Ubuntu 14.04 LTS
+* Python 3
+## File Descriptions
+todo
+## License
+Public Domain, no copyright protection
