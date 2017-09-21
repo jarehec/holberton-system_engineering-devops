e0a11f78af848c93c2a834102fdad85b57822e1f
jarehec <bone1198@gmail.com>
2017-09-21 00:16:58 +0000

    restart apache2

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # fixes typo in /var/www/html/wp-settings
 
 exec { 'fix typo':
-command  => 'sed \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
+command  => 'sed \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php; service apache2 restart',
 path     => ['/usr/bin', '/bin', '/usr/sbin'],
 provider => 'shell'
 }
