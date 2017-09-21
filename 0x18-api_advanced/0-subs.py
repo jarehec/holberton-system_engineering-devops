48e3e40b890e72f394fdfe98141f212d7efec0be
jarehec <bone1198@gmail.com>
2017-09-21 23:19:34 +0000

    move return into try

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -9,6 +9,6 @@ def number_of_subscribers(subreddit):
     try:
         i = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
                 headers={'User-agent': 'hAxr'}, allow_redirects=False).json()
+        return i.get('data').get('subscribers') if not None else 0
     except:
         return 0
-    return i.get('data').get('subscribers')
