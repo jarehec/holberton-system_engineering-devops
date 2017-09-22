cd561b93f344ff7377edb779bebb3caf0cf26e32
jarehec <bone1198@gmail.com>
2017-09-22 00:01:57 +0000

    initial commit

---
+++ 0x18-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pyhon3
+"""
+prints the top ten titles of a given subreddit
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    try:
+        res = get("https://www.reddit.com/r/{}/top.json".format(subreddit),
+                  headers={'User-agent': 'hAxr'}, allow_redirects=False).json()
+        i = 0
+        for item in res.get('data').get('children'):
+            print(item.get('data').get('title'))
+            i += 1
+            if i > 9:
+                break
+    except:
+        print('None')
