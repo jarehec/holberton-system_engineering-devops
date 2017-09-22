de87ba89e239ad08e0d8632c2c804778c26920e4
jarehec <bone1198@gmail.com>
2017-09-22 00:36:20 +0000

    using params in get req

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -8,12 +8,9 @@ from requests import get
 def top_ten(subreddit):
     try:
         res = get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
-                  headers={'User-agent': 'hAxr'}, allow_redirects=False).json()
-        i = 0
-        for item in res.get('data').get('children'):
+                  headers={'User-agent': 'hAxr'}, params={'limit': 10},
+                  allow_redirects=False).json()
+        for item in res['data']['children']:
             print(item.get('data').get('title'))
-            i += 1
-            if i > 9:
-                break
     except:
         print('None')
