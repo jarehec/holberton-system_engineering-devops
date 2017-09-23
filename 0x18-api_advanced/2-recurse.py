52d597641b6dc17a3afc67a21c019fc730d485c0
jarehec <bone1198@gmail.com>
2017-09-23 07:25:21 +0000

    replaced append method with +=

--- 0x18-api_advanced/2-recurse.py
+++ 0x18-api_advanced/2-recurse.py
@@ -5,16 +5,17 @@ returns a list of all titles for a given subreddit
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], after=''):
+def recurse(subreddit, hot_list=[], after=None):
     try:
         url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
         res = get(url, headers={'User-agent': 'hAxr'}, params={'after': after},
                   allow_redirects=False).json()
         after = res['data']['after']
+        print(after)
         for item in res['data']['children']:
             hot_list.append(item['data']['title'])
         if after is not None:
-            hot_list.append(recurse(subreddit, hot_list, res['data']['after']))
+            hot_list += recurse(subreddit, hot_list, after)
         return hot_list if len(hot_list) > 0 else None
     except:
         return None
