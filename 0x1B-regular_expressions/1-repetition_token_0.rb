725b38189f20102517def1f7ac340305bd6db328
jarehec <bone1198@gmail.com>
2017-10-12 06:09:59 +0000

    fixed commands

--- 0x1B-regular_expressions/1-repetition_token_0.rb
+++ 0x1B-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# regular expression that matches hbtn where 't' can be repeated
-puts ARGV[0].scan(/hbt*n/).join
+# regular expression that matches hbtn where 't' can be repeated 2-5 times
+puts ARGV[0].scan(/hbt{2,5}n/).join
--- 0x1B-regular_expressions/2-repetition_token_1.rb
+++ 0x1B-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# regular expression that matches hbtn where 't' is optional
-puts ARGV[0].scan(/hbt{0,1}n/).join
+# regular expression that matches hbtn where 'b' is optional
+puts ARGV[0].scan(/hb?tn/).join
